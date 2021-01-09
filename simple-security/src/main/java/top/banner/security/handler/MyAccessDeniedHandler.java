6cd3602521ef411e6effa1d18846773632e015b3
bannerxu <bannerxu@163.com>
2021-01-10 00:14:58 +0800

    重新整理一个模块处理，方便新模块的编写

--- simple-security/src/main/java/top/banner/security/handler/MyAccessDeniedHandler.java
+++ simple-security/src/main/java/top/banner/security/handler/MyAccessDeniedHandler.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,7 +12,7 @@ import java.io.PrintWriter;
 public class MyAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
-                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+                       AccessDeniedException accessDeniedException) throws IOException {
         response.setStatus(HttpStatus.FORBIDDEN.value());
         response.setHeader("Content-Type", "application/json;charset=utf-8");
         PrintWriter writer = response.getWriter();
--- simple-security/src/main/java/top/banner/security/service/MyServiceImpl.java
+++ simple-security/src/main/java/top/banner/security/service/MyServiceImpl.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
