912e121dc165732a42750333674bf333cc5173b2
banner <1935599727@qq.com>
2022-04-27 00:15:53 +0800

    ss

---
+++ security/src/main/java/top/banner/security/config/IgnoreUrlsConfig.java
@@ -0,0 +1,23 @@
+package top.banner.security.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * SpringSecurity白名单资源路径配置
+ * Created by macro on 2018/11/5.
+ */
+@Getter
+@Setter
+@EnableAutoConfiguration
+@ConfigurationProperties(prefix = "secure.ignored")
+public class IgnoreUrlsConfig {
+
+    private List<String> urls = new ArrayList<>();
+
+}
